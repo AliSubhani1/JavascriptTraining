af29d349c7c44bb2fc943c88bdb0d0ee78c75500
Ali Subhani <ali.subhani@tintash.com>
2021-11-26 08:30:25 +0500

    data manipulation practice

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnCloseModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnOpenModal);
-function openModal() {
-  console.log("button clicked");
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-}
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener("click", openModal);
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1"); //get element by id is same as query selector
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-function closeModel() {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-}
-btnCloseModal.addEventListener("click", closeModel);
-
-overlay.addEventListener("click", closeModel);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    if (!modal.classList.contains("hidden")) {
-      closeModel();
-    }
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player-active");
+    player1El.classList.toggle("player--active");
   }
 });
