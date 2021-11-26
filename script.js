858e0081a85818ce832df0172ec537910e8fa513
Ali Subhani <ali.subhani@tintash.com>
2021-11-26 11:48:54 +0500

    rest pattern

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ const restaurant = {
       open: 0,
       close: 24,
     },
+    orderPizza: function (mainIngredient, ...otherIngredients) {
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+    },
   },
   order: function (starterIndex, mainIndex) {
     return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -31,62 +35,16 @@ const restaurant = {
   },
 };
 
-const arr = [2, 3, 4];
-const [x, y, z] = arr;
-console.log(x, y, z);
-console.log(arr);
+const { sat, ...others } = restaurant.openingHours;
+console.log(others);
 
-let [first, second] = restaurant.categories;
-console.log(first, second);
-[second, first] = [first, second];
-console.log(first, second);
-
-console.log("----OBJECT DESTRUCTURING----");
-
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
-
-const {
-  name: RestaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(RestaurantName, hours, tags);
-
-restaurant.orderDelivery({
-  time: "22:30",
-  address: "cekndnd",
-  mainIndex: 2,
-  starterIndex: 2,
-});
-
-const arrr = [7, 8, 9];
-const badNewArr = [1, 2, arrr[0], arrr[1], arrr[2]];
-console.log(badNewArr);
-const newArr = [1, 2, ...arrr];
-console.log(newArr);
-console.log(...newArr);
-const newMenu = [...restaurant.mainMenu, "gocci"];
-console.log(newMenu);
-console.log(...newMenu);
-
-const maninMenuCopy = [...restaurant.mainMenu];
-
-const menu = [...restaurant.mainMenu, ...restaurant.startMenu];
-console.log(menu);
-
-const str = "jones";
-const letters = [...str, "", "g."];
-console.log(letters);
-
-// const ingredients = [
-//   prompt("let's make pasta! Ingredient 1?"),
-//   prompt("Ingredient 2?"),
-//   prompt("Ingredient 3?"),
-// ];
-// console.log(ingredients);
-// restaurant.orderPasta(...ingredients);
-
-//objects
-const newRestaurant = { founder: "abc", ...restaurant };
-console.log(newRestaurant);
+const add = function (...numbers) {
+  let sum = 0;
+  for (number in numbers) {
+    sum += numbers[number];
+  }
+  console.log(sum);
+};
+add(2, 3);
+add(3, 4, 5, 6, 7, 7);
+add(1, 2, 3, 4, 5, 6, 7, 7);
