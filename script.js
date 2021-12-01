501bfa06ee792e501c4ea911b3b92f9a55ebe15c
Ali Subhani <ali.subhani@tintash.com>
2021-12-01 22:29:06 +0500

    practiced js promises and async wait

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
 "use strict";
 
-let add = (firstNumber, secondNumber) => {
-  return new Promise((resolve, reject) => {
-    let sum = firstNumber + secondNumber;
-    if (sum) {
-      resolve(sum);
-    } else {
-      reject(Error("sum not calculated"));
-    }
-  });
+const onMyBirthday = (isFriendSick) => {
+  return new Promise(
+    (resolve, reject) => {
+      setTimeout(() => {
+        if (!isFriendSick) {
+          resolve(1);
+        } else {
+          reject(0);
+        }
+      });
+    },
+
+    2000
+  );
 };
-let minus = (firstNumber, secondNumber) => {
-  return new Promise((resolve, reject) => {
-    let subtract = firstNumber - secondNumber;
-    if (subtract) {
-      resolve(subtract);
-    } else {
-      reject(Error("Numbers not subtracted"));
-    }
-  });
-};
-add(2, 5)
+
+onMyBirthday(false)
   .then((result) => {
-    return add(result, 50);
+    console.log(`I have ${result} cakes`);
   })
-  .then((result) => {
-    return add(result, 50);
-  })
-  .then((result) => {
-    return add(result, 50);
-  })
-  .then((result) => {
-    return add(result, 50);
-  })
-  .then((result) => {
-    return minus(result, 10);
-    //return minus(result, undefined);
-  })
-  .catch((message) => {
-    console.log(message);
+  .catch((result) => {
+    console.log(`I have ${result} cakes`);
   });
