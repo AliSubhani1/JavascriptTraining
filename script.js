15db9716eaba12dc15029bca062816ce7eebbf82
Ali Subhani <ali.subhani@tintash.com>
2021-12-01 22:42:50 +0500

    js promises practice

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 "use strict";
 
-const onMyBirthday = (isFriendSick) => {
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        if (!isFriendSick) {
-          resolve(1);
-        } else {
-          reject(0);
-        }
-      });
-    },
-
-    2000
-  );
+const enterNumber = (number) => {
+  return new Promise((resolve, reject) => {
+    const userNumber = Number(window.prompt("Enter number 1-6"));
+    const randomNumber = Math.floor(Math.random() * 7);
+    if (isNaN(userNumber)) {
+      reject("Wrong input type");
+    } else if (userNumber < 1 || userNumber > 6) {
+      reject("Incorrect number entered");
+    } else if (userNumber != randomNumber) {
+      resolve("Wrong guess");
+    } else {
+      reject("Correct guess!");
+    }
+  });
 };
 
-onMyBirthday(false)
-  .then((result) => {
-    console.log(`I have ${result} cakes`);
-  })
-  .catch((result) => {
-    console.log(`I have ${result} cakes`);
-  });
+const start = () => {
+  enterNumber()
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+};
+start();
