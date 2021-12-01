0efe11e0f8f474314e34381971e1e2c881f3089f
Ali Subhani <ali.subhani@tintash.com>
2021-12-01 21:58:31 +0500

    practiced js promises

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 "use strict";
 
-let p = new Promise((resolve, reject) => {
-  let a = Math.floor(Math.random() * 3);
-  if (a == 2) {
-    resolve("PASSED");
-  } else {
-    reject("FAILED");
-  }
-});
-
-p.then((message) => {
-  console.log(`promised has been completed: ${message}`);
-}).catch((message) => {
-  console.log(`promise has not been completed ${message}`);
-});
+let add = (firstNumber, secondNumber) => {
+  return new Promise((resolve, reject) => {
+    let sum = firstNumber + secondNumber;
+    if (sum) {
+      resolve(sum);
+    } else {
+      reject(Error("sum not calculated"));
+    }
+  });
+};
+let minus = (firstNumber, secondNumber) => {
+  return new Promise((resolve, reject) => {
+    let subtract = firstNumber - secondNumber;
+    if (subtract) {
+      resolve(subtract);
+    } else {
+      reject(Error("Numbers not subtracted"));
+    }
+  });
+};
+add(2, 5)
+  .then((result) => {
+    return add(result, 50);
+  })
+  .then((result) => {
+    return add(result, 50);
+  })
+  .then((result) => {
+    return add(result, 50);
+  })
+  .then((result) => {
+    return add(result, 50);
+  })
+  .then((result) => {
+    return minus(result, 10);
+    //return minus(result, undefined);
+  })
+  .catch((message) => {
+    console.log(message);
+  });
